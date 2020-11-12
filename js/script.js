80111f4867966e59c2ac5dafaa0a67a73c5fec29
Tomasz Wojciechowski <tomaszwojciechowski244@gmail.com>
2020-11-12 16:16:18 +0100

    create logic in script.js file

---
+++ js/script.js
@@ -0,0 +1,32 @@
+const titleClickHandler = function (event) {
+  event.preventDefault();
+
+  // remove class 'active' from all article links
+  const activeLinks = document.querySelectorAll('.titles a.active');
+  for (let activeLink of activeLinks) {
+    activeLink.classList.remove('active');
+  }
+
+  // add class 'active' to clicked link
+  this.classList.add('active');
+
+  // remove class 'active' from all articles
+  const activeArticles = document.querySelectorAll('.post');
+  for (let activeArticle of activeArticles) {
+    activeArticle.classList.remove('active');
+  }
+
+  // get 'href' attribute from clicked link
+  const articleSelector = this.getAttribute('href');
+
+  // find the correct article using the selector (value of 'href' attribute)
+  const targetArticle = document.querySelector(articleSelector);
+
+  // add class 'active to the correct article
+  targetArticle.classList.add('active');
+}
+
+const links = document.querySelectorAll('.titles a');
+for (let link of links) {
+  link.addEventListener('click', titleClickHandler);
+}
